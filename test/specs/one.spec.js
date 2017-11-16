711bb141ac67993629d7d89c4736eed2926a21a2
Arik Jones <tnypxl00@icloud.com>
2017-11-16 11:44:47 -0600

    Add an example spec

---
+++ test/specs/one.spec.js
@@ -0,0 +1,16 @@
+// var webdriverio = require('webdriverio');
+// var browser = webdriverio
+//   .remote({ host: 'localhost', port: 4444 })
+//   .init({ browserName: 'firefox' });
+
+describe('DuckDuckGo search', function() {
+  it('searches for WebdriverIO', function() {
+      browser.url('https://duckduckgo.com/');
+      browser.setValue('#search_form_input_homepage', 'WebdriverIO');
+      browser.click('#search_button_homepage');
+
+      var title = browser.getTitle();
+      console.log('Title is: ' + title);
+      // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+  });
+});
