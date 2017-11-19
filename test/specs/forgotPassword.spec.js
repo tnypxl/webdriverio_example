adc31d5c915ffa20a3d06756f00eadc45b771f02
Arik Jones <tnypxl00@icloud.com>
2017-11-18 19:12:07 -0600

    Validate URL

--- test/specs/forgotPassword.spec.js
+++ test/specs/forgotPassword.spec.js
@@ -11,6 +11,7 @@ describe('Forgot Password', () => {
   it('redirects to confirmation page when I retrieve my password', () => {
     ForgotPasswordPage.open();
     ForgotPasswordPage.retrievePasswordFor('someone@example.derp');
+    expect(browser.getUrl()).to.eq('http://the-internet.herokuapp.com/email_sent');
     expect(EmailSentPage.confirmationMessage.getText()).to.contain("Your e-mail's been sent!");
   });
 
