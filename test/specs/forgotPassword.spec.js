912979c0bd2bac069d4c80056bedadcd1be6a7b8
Arik Jones <tnypxl00@icloud.com>
2017-11-18 19:03:37 -0600

    Add forgetPassword spec

---
+++ test/specs/forgotPassword.spec.js
@@ -0,0 +1,17 @@
+// var expect = require('chai').expect;
+// var ForgotPasswordPage = require('../site/forgotPassword.page');
+// var EmailSentPage = require('../site/emailSent.page');
+
+import { expect } from "chai";
+import ForgotPasswordPage from "../site/forgotPassword.page";
+import EmailSentPage from "../site/emailSent.page";
+
+describe('Forgot Password', () => {
+
+  it('redirects to confirmation page when I retrieve my password', () => {
+    ForgotPasswordPage.open();
+    ForgotPasswordPage.retrievePasswordFor('someone@example.derp');
+    expect(EmailSentPage.confirmationMessage.getText()).to.contain("Your e-mail's been sent!");
+  });
+
+});
